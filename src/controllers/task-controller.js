10e7325e3c2f7438f9ae024ad0112587f94a2ef1
Eduarda Tavares <erteduarda@gmail.com>
2026-03-19 23:47:02 -0300

    add `tasksListPerId`

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -1,8 +1,16 @@
 class TaskRouter {
-  static async listTasks(req, res, next) {
+  static async tasksList(req, res, next) {
     try {
       const tasks = ["Task 1", "Task 2"];
       res.status(200).send(tasks);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+
+  static async tasksListPerId(req, res, next) {
+    try {
+      res.status(200).send("Teste");
     } catch (error) {
       res.status(500).send(error);
     }
--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -3,6 +3,7 @@ import TaskRouter from "../controllers/task-controller.js";
 
 const router = express.Router();
 
-router.get("/tasks", TaskRouter.listTasks);
+router.get("/tasks", TaskRouter.tasksList);
+router.get("/tasks:id", TaskRouter.tasksListPerId);
 
 export default router;
